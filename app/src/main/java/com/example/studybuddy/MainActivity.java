e30c618828b2368a86e895baf3c4ec48d37250af
unknown <beam722@naver.com>
2024-11-17 00:07:07 +0900

    navigation 만들었어용

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -1,24 +1,76 @@
 package com.example.studybuddy;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        if (savedInstanceState == null) {
+            transferTo(ClassChatListFragment.newInstance("param1", "param2"));
+            bottomNavigationView.setSelectedItemId(R.id.chatPage);
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+
+                if (itemId == R.id.chatPage) {
+                    transferTo(ClassChatListFragment.newInstance("param1", "param2"));
+                    return true;
+                }
+
+                if (itemId == R.id.timerPage) {
+                    transferTo(MyTimerFragment.newInstance("param1", "param2"));
+                    return true;
+                }
+
+                if (itemId == R.id.notificationPage) {
+                    transferTo(MyNotificationsFragment.newInstance("param1", "param2"));
+                    return true;
+                }
+
+                if (itemId == R.id.personalPage) {
+                    transferTo(MyProfileFragment.newInstance("param1", "param2"));
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+            }
         });
     }
+
+    private void transferTo(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
 }
+
